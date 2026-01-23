e3823f8e53e9f5ecd67d494ce2157021ab949bb4
m-silacukur <meryemsilacukur@gmail.com>
2026-01-24 01:17:52 +0300

    kurulum dosyaları

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-pink-600">
+        Okey Honey!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
